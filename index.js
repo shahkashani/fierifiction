3bc0e7957c0bc8ad96aa2d7223b142f0d5910470
Shah Kashani <skashani@gmail.com>
2019-04-27 15:43:53 -0400

    Fix freezing video

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ class FieriFiction {
     const loop = useLoop || this.getRandom(this.loops);
     console.log(`\n🎷 Adding music: ${loop}`);
     const len = Math.floor(this.getVideoLength(input) - trim);
-    const cmd = `ffmpeg -i "${input}" -vf trim=0:15 -filter_complex "amovie='${loop}':loop=999,loudnorm[s];[0][s]amix=duration=shortest" -t ${len} -y "temp-${input}" && rm "${input}" && mv "temp-${input}" "${input}"`;
+    const cmd = `ffmpeg -i "${input}" -filter_complex "amovie='${loop}':loop=999,loudnorm[s];[0][s]amix=duration=shortest" -t ${len} -y "temp-${input}" && rm "${input}" && mv "temp-${input}" "${input}"`;
 
     this.execCmd(cmd);
   }
