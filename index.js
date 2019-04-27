1c00f32c5b141245fa5c85bff6e8246ee734c670
Shah Kashani <skashani@gmail.com>
2019-04-26 23:00:55 -0400

    Well, it works

--- index.js
+++ index.js
@@ -1,29 +1,163 @@
 const tumblr = require('tumblr.js');
+const request = require('request-promise');
+const { writeFileSync, readFileSync, unlinkSync } = require('fs');
+const { exec } = require('shelljs');
+
+const WATSON_URL =
+  'https://stream.watsonplatform.net/text-to-speech/api/v1/synthesize';
 
 class FieriFiction {
   constructor({
-    consumerKey = null,
-    consumerSecret = null,
-    token = null,
-    tokenSecret = null,
-    blogName = null,
-    textGeneratorUrl = null
+    tumblrConsumerKey = null,
+    tumblrConsumerSecret = null,
+    tumblrTokenKey = null,
+    tumblrTokenSecret = null,
+    tumblrBlogName = null,
+    textGeneratorUrl = null,
+    audioGeneratorUrl = null,
+    watsonApiKey = null,
+    textLength = 100
   }) {
     this.client = tumblr.createClient({
-      token,
-      consumer_key: consumerKey,
-      consumer_secret: consumerSecret,
-      token_secret: tokenSecret,
+      token: tumblrTokenKey,
+      token_secret: tumblrTokenSecret,
+      consumer_key: tumblrConsumerKey,
+      consumer_secret: tumblrConsumerSecret,
       returnPromises: true
     });
-    this.reblog = reblog;
-    this.tags = tags;
-    this.blogName = blogName;
+
+    this.blogName = tumblrBlogName;
     this.textGeneratorUrl = textGeneratorUrl;
+    this.audioGeneratorUrl = audioGeneratorUrl;
+    this.textLength = textLength;
+    this.watsonApiKey = watsonApiKey;
   }
 
-  postStory(text, tags) {
-    console.log(text, tags);
+  captionsToString(captions) {
+    return captions
+      .join(' ')
+      .replace(/\\n/gi, ' ')
+      .replace(/\s{2,}/, ' ');
+  }
+
+  getFullSentences(string) {
+    const match = string.match(/[.!?]/gi);
+    const lastIndex = string.lastIndexOf(match[match.length - 1]);
+    return string.slice(0, lastIndex + 1);
+  }
+
+  execCmd(cmd) {
+    const result = exec(cmd, { silent: true });
+    if (result.code !== 0) {
+      console.log(`🐞 Oops: ${result.stderr}\n> ${cmd}`);
+    }
+    return result;
+  }
+
+  createVideo(image, audio, output) {
+    console.log('\n📽️ Generating video');
+
+    return this.execCmd(
+      `ffmpeg -i "${audio}" -ignore_loop 0 -i "${image}" -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" -shortest -strict -2 -c:v libx264 -threads 4 -c:a aac -b:a 192k -pix_fmt yuv420p -shortest -y "${output}"`
+    );
+  }
+
+  async reblogPost(reblogPostId, reblogName, tags, text) {
+    console.log('\n🔄 Reblogging text post');
+    const postInfo = await this.client.blogPosts(reblogName, {
+      id: reblogPostId
+    });
+    const reblogKey = postInfo.posts[0].reblog_key;
+    const response = await this.client.reblogPost(this.blogName, {
+      id: reblogPostId,
+      tags: tags.join(','),
+      reblog_key: reblogKey,
+      comment: text
+    });
+    return response;
+  }
+
+  async generateAudio(text, output) {
+    console.log('\n🕋 Talking to Watson');
+
+    const headers = {
+      'Content-Type': 'application/json',
+      Accept: 'audio/mp3'
+    };
+
+    const dataString = JSON.stringify({
+      text
+    });
+
+    const options = {
+      encoding: null,
+      url: WATSON_URL,
+      method: 'POST',
+      headers: headers,
+      body: dataString,
+      auth: {
+        user: 'apikey',
+        pass: this.watsonApiKey
+      }
+    };
+
+    const response = await request(options);
+    writeFileSync(output, response);
+  }
+
+  async generateStory(captions) {
+    console.log('\n💭 Talking to Dreamscape');
+    const input = this.captionsToString(captions);
+    const req = await request({
+      uri: this.textGeneratorUrl,
+      qs: {
+        q: input,
+        length: this.textLength
+      },
+      json: true
+    });
+    return this.getFullSentences(req.output || '');
+  }
+
+  async postVideo(story, image, tags) {
+    const mp3 = `${image}.mp3`;
+    const mp4 = `${image}.mp4`;
+
+    await this.generateAudio(story, mp3);
+    this.createVideo(image, mp3, mp4);
+
+    const video = readFileSync(mp4);
+    const videoPost = await this.client.createVideoPost(this.blogName, {
+      data64: video.toString('base64'),
+      tags: tags.join(','),
+      caption: story
+    });
+
+    console.log(
+      `👀 Video uploaded to https://${this.blogName}.tumblr.com/post/${
+        videoPost.id
+      }`
+    );
+    console.log('👋 Wrapping up!');
+    unlinkSync(mp3);
+    unlinkSync(mp4);
+  }
+
+  async post(image, captions, tags = [], reblogInfo = null) {
+    try {
+      const story = await this.generateStory(captions);
+      if (reblogInfo) {
+        await this.reblogPost(
+          reblogInfo.postId,
+          reblogInfo.blogName,
+          tags,
+          story
+        );
+      }
+      await this.postVideo(story, image, tags);
+    } catch (err) {
+      console.log(`💥 Something borked: ${err}`);
+    }
   }
 }
 
